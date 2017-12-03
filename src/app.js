a31cb457b31ee06201ba06df989a6747dc512eb5
Roy Talyosef <roy@productionmap.com>
2017-12-03 19:52:58 +0200

    rename list method

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@
 let aws = require("aws-sdk");
 
 function listBuckets(action) {
+    console.log("!!!", action.params.REGION);
     return new Promise((resolve, reject) => {
         aws.config.update({
             region: action.params.REGION,
