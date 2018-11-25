b795bc6fa4619a4074f716aba050a1a73e0f9912
kadosh1000 <kadosh10001@gmail.com>
2018-11-25 12:37:45 +0200

    * fix plugin structure

--- src/app.js
+++ src/app.js
@@ -141,7 +141,7 @@ function deleteObject(action) {
     });
 }
 
-let functions = {
+module.exports = {
     listBuckets: listBuckets,
     createBucket: createBucket,
     uploadFileToBucket: uploadFileToBucket,
@@ -149,25 +149,3 @@ let functions = {
     listObjectsInBucket: listObjects,
     deleteObject: deleteObject
 };
-
-function main(argv) {
-    if (argv.length < 3) {
-        console.log({ err: "not enough parameters" });
-        // Invalid Argument
-        // Either an unknown option was specified, or an option requiring a value was provided without a value.
-        process.exit(9);
-    }
-    let action = JSON.parse(argv[2]);
-    functions[action.method.name](action)
-        .then((res) => {
-            console.log(res);
-            process.exit(0); // Success
-        }).catch((err) => {
-            console.log("An error occured", err);
-            // Uncaught Fatal Exception
-            // There was an uncaught exception, and it was not handled by a domain or an 'uncaughtException' event handler.
-            process.exit(1); // Failure
-        });
-}
-
-main(process.argv);
